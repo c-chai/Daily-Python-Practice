e71d2a4c5a5b70dd32c4779898ff5fc09ec47481
Carmen Chai <150074877+c-chai@users.noreply.github.com>
2023-12-22 10:54:07 -0800

    Wild_Madlib.py

--- Wild_Madlib.py
+++ Wild_Madlib.py
@@ -1,18 +1,30 @@
-# This madlib should leave you smiling 
 # Title: Unexpected Day at the Zoo
 
-adj1 = input("Adjective: ")
-adj2 = input("Another adjective: ")
-silly = input("A silly word: ")
-animal = input("An animal: ")
-transporation_type = input("A type of transporation: ")
-funny_sound = input("A funny sound: ")
-unusual_color = input("An unusual color: ")
-food_item = input("A ridiculous food item: ")
-celebrity = input("A famous celebrity (fictional):  ")
-strange_object = input("A strange object: ")
-activity = input("A hilarious activity: ")
-odd_job = input("An oddly specific job: ")
+def get_input(prompt, input_type=str):
+    '''
+    This function will catch any numeric inputs and tell the user no numbers. 
+    '''    
+    while True:
+        user_input = input(prompt)
+        try:
+            if user_input.isnumeric():
+                raise ValueError ("Aiyo, no numbers. ")
+            return user_input
+        except ValueError as e: 
+            print(e)
+
+adj1 = get_input("Adjective: ")
+adj2 = get_input("Another adjective: ")
+silly = get_input("A silly word: ")
+animal = get_input("An animal: ")
+transporation_type = get_input("A type of transporation: ")
+funny_sound = get_input("A funny sound: ")
+unusual_color = get_input("An unusual color: ")
+food_item = get_input("A ridiculous food item: ")
+celebrity = get_input("A famous celebrity (fictional):  ")
+strange_object = get_input("A strange object: ")
+activity = get_input("A hilarious activity: ")
+odd_job = get_input("An oddly specific job: ")
 
 p1 = f"Once upon a time, in the middle of a {adj1} day, a(n) {adj2} {animal} escaped from its enclosure at the zoo. The zookeeper shouted, 'Holy {silly}! We need to catch it!' So, they hopped into their trusty golf cart, making a loud {funny_sound} as they sped away."
 
