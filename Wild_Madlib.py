9b4fcb7290a152d2b76abcde6be082be8f545e77
Carmen Chai <150074877+c-chai@users.noreply.github.com>
2023-12-22 10:33:19 -0800

    Add files via upload

---
+++ Wild_Madlib.py
@@ -0,0 +1,27 @@
+# This madlib should leave you smiling 
+# Title: Unexpected Day at the Zoo
+
+adj1 = input("Adjective: ")
+adj2 = input("Another adjective: ")
+silly = input("A silly word: ")
+animal = input("An animal: ")
+transporation_type = input("A type of transporation: ")
+funny_sound = input("A funny sound: ")
+unusual_color = input("An unusual color: ")
+food_item = input("A ridiculous food item: ")
+celebrity = input("A famous celebrity (fictional):  ")
+strange_object = input("A strange object: ")
+activity = input("A hilarious activity: ")
+odd_job = input("An oddly specific job: ")
+
+p1 = f"Once upon a time, in the middle of a {adj1} day, a(n) {adj2} {animal} escaped from its enclosure at the zoo. The zookeeper shouted, 'Holy {silly}! We need to catch it!' So, they hopped into their trusty golf cart, making a loud {funny_sound} as they sped away."
+
+p2 = f"As they chased the animal, it suddenly turned a {unusual_color} color, which was absolutely unexpected. 'I\'ve never seen anything like it!' exclaimed the zookeeper, munching on a {food_item} for stress relief."
+
+p3 = f"Just then, {celebrity} appeared out of nowhere, riding a {transporation_type} and wielding a {strange_object}. 'I\'m here to help!' they declared."
+
+p4 = f"Together, they all started {activity}, which, surprisingly, calmed the animal down. It turned out that the animal was just looking for someone to interact with!"
+
+p5 = f"In the end, the zoo hired a {odd_job} to ensure this hilarious situation would never happen again. And everyone lived happily ever after, occasionally turning {unusual_color} for no reason at all." 
+
+print(p1, p2, p3, p4, p5)
