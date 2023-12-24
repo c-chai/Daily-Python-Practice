540c49dd4489a8d4f1444406c82e8184a7a34e6f
Carmen Chai <150074877+c-chai@users.noreply.github.com>
2023-12-24 08:55:35 -0800

    Add files via upload

---
+++ Cleaning_Strings.py
@@ -0,0 +1,11 @@
+# Remove special symbols / punctuation / numbers / spaces from a string
+# Also format it correctly with title() 
+import re # Very useful here 
+import string
+
+sentence = "#(*1) whY aRe YoU THE $ w@ay ^ thaT y-ou ARE? "
+# Removes all characteres except alphabets and spaces
+cleaned_sentence = re.sub(r'[^A-Za-z ]+', '', sentence)
+# Removes any trailing and leading spaces, and then capitlizes the first letter of sentence
+formatted_sentence = cleaned_sentence.strip().capitalize()
+print(formatted_sentence)
