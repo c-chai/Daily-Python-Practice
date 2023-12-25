a6367e0b839627fbaa5f36b9daba783229871883
Carmen Chai <150074877+c-chai@users.noreply.github.com>
2023-12-25 08:06:07 -0800

    Cleaning_Strings.py

--- Cleaning_Strings.py
+++ Cleaning_Strings.py
@@ -6,6 +6,6 @@ import string
 sentence = "#(*1) whY aRe YoU THE $ w@ay ^ thaT y-ou ARE? "
 # Removes all characteres except alphabets and spaces
 cleaned_sentence = re.sub(r'[^A-Za-z ]+', '', sentence)
-# Removes any trailing and leading spaces, and then capitlizes the first letter of sentence
+# Removes any trailing and leading spaces, and then capitalizes the first letter of sentence
 formatted_sentence = cleaned_sentence.strip().capitalize()
 print(formatted_sentence)
