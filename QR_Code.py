0e58f7a9e7cc3ae4c6e56770074e7c8839fa52ed
Carmen Chai <150074877+c-chai@users.noreply.github.com>
2023-12-27 13:36:41 -0800

    Add files via upload

---
+++ QR_Code.py
@@ -0,0 +1,21 @@
+# Create a function that collects a text and converts it to a qrcode
+# Save the qrcode as an image
+
+import qrcode
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save('qrimg.png')
+
+generate_qrcode('marinelife')
